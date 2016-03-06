c6b31708b34ee1eb604c7bd78abf8e7f1ce36422
Mark Harmstone <mark@harmstone.com>
2016-03-06 20:05:16 +0000

    create_file: set FileObject->Vpb

--- src/create.c
+++ src/create.c
@@ -2195,8 +2195,13 @@ static NTSTATUS STDCALL create_file(PDEVICE_OBJECT DeviceObject, PIRP Irp, LIST_
         FileObject->Flags |= FO_CACHE_SUPPORTED;
     
 exit:
-    if (!NT_SUCCESS(Status) && Status != STATUS_OBJECT_NAME_NOT_FOUND && Status != STATUS_OBJECT_PATH_NOT_FOUND)
-        TRACE("returning %08x\n", Status);
+    if (NT_SUCCESS(Status)) {
+        if (!FileObject->Vpb)
+            FileObject->Vpb = DeviceObject->Vpb;
+    } else {
+        if (Status != STATUS_OBJECT_NAME_NOT_FOUND && Status != STATUS_OBJECT_PATH_NOT_FOUND)
+            TRACE("returning %08x\n", Status);
+    }
     
     return Status;
 }
