f997443874e38bbeb446db77f86e253b5d6ddfce
Mark Harmstone <mark@harmstone.com>
2016-03-19 21:30:30 +0000

    support FSCTL_BTRFS_GET_FILE_IDS ioctl

---
+++ src/btrfsioctl.h
@@ -0,0 +1,11 @@
+#ifndef BTRFSIOCTL_H_DEFINED
+#define BTRFSIOCTL_H_DEFINED
+
+#define FSCTL_BTRFS_GET_FILE_IDS CTL_CODE(FILE_DEVICE_UNKNOWN, 0x829, METHOD_OUT_DIRECT, FILE_ANY_ACCESS)
+
+typedef struct {
+    UINT64 subvol;
+    UINT64 inode;
+} btrfs_get_file_ids;
+
+#endif
