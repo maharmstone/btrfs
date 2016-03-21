e515508bb9732a73718aa2f271e9801a2b49e474
Mark Harmstone <mark@harmstone.com>
2016-03-21 18:58:59 +0000

    shellext: call FSCTL_BTRFS_GET_FILE_IDS to see if directory is subvol

--- src/shellext/iconoverlay.cpp
+++ src/shellext/iconoverlay.cpp
@@ -1,5 +1,29 @@
 #include <windows.h>
 #include "iconoverlay.h"
+#include "../btrfsioctl.h"
+
+typedef struct _IO_STATUS_BLOCK {
+  union {
+    NTSTATUS Status;
+    PVOID Pointer;
+  } DUMMYUNIONNAME;
+  ULONG_PTR Information;
+} IO_STATUS_BLOCK, *PIO_STATUS_BLOCK;
+
+typedef VOID
+(NTAPI *PIO_APC_ROUTINE)(
+  IN PVOID ApcContext,
+  IN PIO_STATUS_BLOCK IoStatusBlock,
+  IN ULONG Reserved);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+NTSYSCALLAPI NTSTATUS NTAPI NtFsControlFile(HANDLE FileHandle, HANDLE Event, PIO_APC_ROUTINE ApcRoutine, PVOID ApcContext, PIO_STATUS_BLOCK IoStatusBlock, ULONG FsControlCode, PVOID InputBuffer, ULONG InputBufferLength, PVOID OutputBuffer, ULONG OutputBufferLength);
+#ifdef __cplusplus
+}
+#endif
+#define STATUS_SUCCESS 0
 
 extern HMODULE module;
 
@@ -45,7 +69,26 @@ HRESULT __stdcall BtrfsIconOverlay::GetPriority(int *pPriority) {
 }
 
 HRESULT __stdcall BtrfsIconOverlay::IsMemberOf(PCWSTR pwszPath, DWORD dwAttrib) {
-    // FIXME
+    HANDLE h;
+    NTSTATUS Status;
+    IO_STATUS_BLOCK iosb;
+    btrfs_get_file_ids bgfi;
     
-    return S_OK;
+    // FIXME - return S_FALSE for volume root
+     
+    h = CreateFileW(pwszPath, 0, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, NULL);
+    
+    if (h == INVALID_HANDLE_VALUE)
+        return S_FALSE;
+    
+    Status = NtFsControlFile(h, NULL, NULL, NULL, &iosb, FSCTL_BTRFS_GET_FILE_IDS, NULL, 0, &bgfi, sizeof(btrfs_get_file_ids));
+    
+    if (Status != STATUS_SUCCESS) {
+        CloseHandle(h);
+        return S_FALSE;
+    }
+
+    CloseHandle(h);
+    
+    return bgfi.inode == 0x100 ? S_OK : S_FALSE;
 }
