d7a163459c1210f466a55db0e1ad5851668be0fb
Mark Harmstone <mark@harmstone.com>
2016-04-09 14:27:06 +0100

    add pnp stub functions

--- src/pnp.c
+++ src/pnp.c
@@ -1,13 +1,47 @@
 #include "btrfs_drv.h"
 
+static NTSTATUS pnp_cancel_remove_device(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+    FIXME("STUB\n");
+
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+static NTSTATUS pnp_query_remove_device(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+    FIXME("STUB\n");
+
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+static NTSTATUS pnp_remove_device(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+    FIXME("STUB\n");
+
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+static NTSTATUS pnp_start_device(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+    FIXME("STUB\n");
+
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+static NTSTATUS pnp_surprise_removal(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+    FIXME("STUB\n");
+
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+static NTSTATUS pnp_query_device_relations(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
+    FIXME("STUB\n");
+
+    return STATUS_NOT_IMPLEMENTED;
+}
+
 NTSTATUS STDCALL drv_pnp(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
     PIO_STACK_LOCATION IrpSp = IoGetCurrentIrpStackLocation(Irp);
-    device_extension* Vcb = DeviceObject->DeviceExtension;
+//     device_extension* Vcb = DeviceObject->DeviceExtension;
     NTSTATUS Status;
     BOOL top_level;
 
-    FIXME("STUB: pnp\n");
-    
     FsRtlEnterFileSystem();
 
     top_level = is_top_level(Irp);
@@ -16,27 +50,27 @@ NTSTATUS STDCALL drv_pnp(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
     
     switch (IrpSp->MinorFunction) {
         case IRP_MN_CANCEL_REMOVE_DEVICE:
-            TRACE("    IRP_MN_CANCEL_REMOVE_DEVICE\n");
+            Status = pnp_cancel_remove_device(DeviceObject, Irp);
             break;
 
         case IRP_MN_QUERY_REMOVE_DEVICE:
-            TRACE("    IRP_MN_QUERY_REMOVE_DEVICE\n");
+            Status = pnp_query_remove_device(DeviceObject, Irp);
             break;
 
         case IRP_MN_REMOVE_DEVICE:
-            TRACE("    IRP_MN_REMOVE_DEVICE\n");
+            Status = pnp_remove_device(DeviceObject, Irp);
             break;
 
         case IRP_MN_START_DEVICE:
-            TRACE("    IRP_MN_START_DEVICE\n");
+            Status = pnp_start_device(DeviceObject, Irp);
             break;
 
         case IRP_MN_SURPRISE_REMOVAL:
-            TRACE("    IRP_MN_SURPRISE_REMOVAL\n");
+            Status = pnp_surprise_removal(DeviceObject, Irp);
             break;
             
         case IRP_MN_QUERY_DEVICE_RELATIONS:
-            TRACE("    IRP_MN_QUERY_DEVICE_RELATIONS\n");
+            Status = pnp_query_device_relations(DeviceObject, Irp);
             break;
         
         default:
@@ -44,14 +78,18 @@ NTSTATUS STDCALL drv_pnp(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
             break;
     }
 
-//     Irp->IoStatus.Status = Status;
-//     Irp->IoStatus.Information = 0;
+// //     Irp->IoStatus.Status = Status;
+// //     Irp->IoStatus.Information = 0;
+// 
+//     IoSkipCurrentIrpStackLocation(Irp);
+//     
+//     Status = IoCallDriver(Vcb->devices[0].devobj, Irp);
+// 
+// //     IoCompleteRequest(Irp, IO_NO_INCREMENT);
 
-    IoSkipCurrentIrpStackLocation(Irp);
-    
-    Status = IoCallDriver(Vcb->devices[0].devobj, Irp);
+    Irp->IoStatus.Status = Status;
 
-//     IoCompleteRequest(Irp, IO_NO_INCREMENT);
+    IoCompleteRequest(Irp, IO_NO_INCREMENT);
     
     if (top_level) 
         IoSetTopLevelIrp(NULL);
