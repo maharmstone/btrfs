7bd9675fc23547bc9db5d2c47c359b726adebeb8
Mark Harmstone <mark@harmstone.com>
2016-04-10 20:14:21 +0100

    load free space cache

--- src/btrfs.c
+++ src/btrfs.c
@@ -2965,24 +2965,97 @@ static NTSTATUS STDCALL load_chunk_root(device_extension* Vcb) {
     return STATUS_SUCCESS;
 }
 
-static BOOL load_stored_free_space_cache(device_extension* Vcb, chunk* c) {
+static NTSTATUS add_space_entry(chunk* c, UINT64 offset, UINT64 size) {
+    space* s;
+    
+    s = ExAllocatePoolWithTag(PagedPool, sizeof(space), ALLOC_TAG);
+
+    if (!s) {
+        ERR("out of memory\n");
+        return STATUS_INSUFFICIENT_RESOURCES;
+    }
+
+    s->offset = offset;
+    s->size = size;
+    s->type = SPACE_TYPE_FREE;
+    
+    if (IsListEmpty(&c->space))
+        InsertTailList(&c->space, &s->list_entry);
+    else {
+        space* s2 = CONTAINING_RECORD(c->space.Blink, space, list_entry);
+        
+        if (s2->offset < offset)
+            InsertTailList(&c->space, &s->list_entry);
+        else {
+            LIST_ENTRY* le;
+            
+            le = c->space.Flink;
+            while (le != &c->space) {
+                s2 = CONTAINING_RECORD(le, space, list_entry);
+                
+                if (s2->offset > offset) {
+                    InsertTailList(le, &s->list_entry);
+                    return STATUS_SUCCESS;
+                }
+                
+                le = le->Flink;
+            }
+        }
+    }
+    
+    return STATUS_SUCCESS;
+}
+
+static void load_free_space_bitmap(device_extension* Vcb, chunk* c, UINT64 offset, void* data) {
+    RTL_BITMAP bmph;
+    UINT32 i, *dwords = data;
+    ULONG runlength, index;
+    
+    // flip bits
+    for (i = 0; i < Vcb->superblock.sector_size / sizeof(UINT32); i++) {
+        dwords[i] = ~dwords[i];
+    }
+
+    RtlInitializeBitMap(&bmph, data, Vcb->superblock.sector_size * 8);
+
+    index = 0;
+    runlength = RtlFindFirstRunClear(&bmph, &index);
+            
+    while (runlength != 0) {
+        UINT64 addr, length;
+        
+        addr = offset + (index * Vcb->superblock.sector_size);
+        length = Vcb->superblock.sector_size * runlength;
+        
+        add_space_entry(c, addr, length);
+        index += runlength;
+       
+        runlength = RtlFindNextForwardRunClear(&bmph, index, &index);
+    }
+}
+
+static NTSTATUS load_stored_free_space_cache(device_extension* Vcb, chunk* c) {
     KEY searchkey;
     traverse_ptr tp, tp2;
     FREE_SPACE_ITEM* fsi;
-    UINT64 inode, num_sectors, i, generation;
+    UINT64 inode, num_sectors, num_valid_sectors, i, generation;
     INODE_ITEM* ii;
     UINT8* data;
     NTSTATUS Status;
     UINT32 *checksums, crc32;
-#ifdef _DEBUG
     FREE_SPACE_ENTRY* fse;
-    UINT64 num_entries;
-#endif
+    UINT64 size, num_entries, num_bitmaps, extent_length, bmpnum;
+//     LIST_ENTRY* le;
+    
+    // FIXME - does this break if Vcb->superblock.sector_size is not 4096?
     
     TRACE("(%p, %llx)\n", Vcb, c->offset);
     
-    if (Vcb->superblock.generation != Vcb->superblock.cache_generation)
-        return FALSE;
+    // We've already increased the generation by one
+    if (Vcb->superblock.generation - 1 != Vcb->superblock.cache_generation) {
+        // FIXME - delete any entry there already?
+        return STATUS_NOT_FOUND;
+    }
     
     searchkey.obj_id = FREE_SPACE_CACHE_ID;
     searchkey.obj_type = 0;
@@ -2991,110 +3064,117 @@ static BOOL load_stored_free_space_cache(device_extension* Vcb, chunk* c) {
     Status = find_item(Vcb, Vcb->root_root, &tp, &searchkey, FALSE);
     if (!NT_SUCCESS(Status)) {
         ERR("error - find_item returned %08x\n", Status);
-        return FALSE;
+        return Status;
     }
     
     if (keycmp(&tp.item->key, &searchkey)) {
-        WARN("(%llx,%x,%llx) not found\n", searchkey.obj_id, searchkey.obj_type, searchkey.offset);
+        TRACE("(%llx,%x,%llx) not found\n", searchkey.obj_id, searchkey.obj_type, searchkey.offset);
         free_traverse_ptr(&tp);
-        return FALSE;
+        return STATUS_NOT_FOUND;
     }
     
     if (tp.item->size < sizeof(FREE_SPACE_ITEM)) {
         WARN("(%llx,%x,%llx) was %u bytes, expected %u\n", tp.item->key.obj_id, tp.item->key.obj_type, tp.item->key.offset, tp.item->size, sizeof(FREE_SPACE_ITEM));
         free_traverse_ptr(&tp);
-        return FALSE;
+        return STATUS_NOT_FOUND;
     }
     
     fsi = (FREE_SPACE_ITEM*)tp.item->data;
     
-    if (fsi->generation != Vcb->superblock.cache_generation) {
-        WARN("cache had generation %llx, expecting %llx\n", fsi->generation, Vcb->superblock.cache_generation);
-        free_traverse_ptr(&tp);
-        return FALSE;
-    }
-    
     if (fsi->key.obj_type != TYPE_INODE_ITEM) {
         WARN("cache pointed to something other than an INODE_ITEM\n");
         free_traverse_ptr(&tp);
-        return FALSE;
-    }
-    
-    if (fsi->num_bitmaps > 0) {
-        WARN("cache had bitmaps, unsure of how to deal with these\n");
-        free_traverse_ptr(&tp);
-        return FALSE;
+        return STATUS_NOT_FOUND;
     }
     
     inode = fsi->key.obj_id;
     
     searchkey = fsi->key;
-#ifdef _DEBUG
+
     num_entries = fsi->num_entries;
-#endif
+    num_bitmaps = fsi->num_bitmaps;
     
     Status = find_item(Vcb, Vcb->root_root, &tp2, &searchkey, FALSE);
     if (!NT_SUCCESS(Status)) {
         ERR("error - find_item returned %08x\n", Status);
         free_traverse_ptr(&tp);
-        return FALSE;
+        return Status;
     }
-    
-    free_traverse_ptr(&tp);
     
     if (keycmp(&tp2.item->key, &searchkey)) {
         WARN("(%llx,%x,%llx) not found\n", searchkey.obj_id, searchkey.obj_type, searchkey.offset);
+        free_traverse_ptr(&tp);
         free_traverse_ptr(&tp2);
-        return FALSE;
+        return STATUS_NOT_FOUND;
     }
     
     if (tp2.item->size < sizeof(INODE_ITEM)) {
         WARN("(%llx,%x,%llx) was %u bytes, expected %u\n", tp2.item->key.obj_id, tp2.item->key.obj_type, tp2.item->key.offset, tp2.item->size, sizeof(INODE_ITEM));
+        free_traverse_ptr(&tp);
         free_traverse_ptr(&tp2);
-        return FALSE;
+        return STATUS_NOT_FOUND;
     }
     
     ii = (INODE_ITEM*)tp2.item->data;
     
-    data = ExAllocatePoolWithTag(PagedPool, ii->st_size, ALLOC_TAG);
+    if (ii->st_size == 0) {
+        ERR("inode %llx had a length of 0\n", inode);
+        free_traverse_ptr(&tp);
+        free_traverse_ptr(&tp2);
+        return STATUS_NOT_FOUND;
+    }
+    
+    size = sector_align(ii->st_size, Vcb->superblock.sector_size);
+    
+    data = ExAllocatePoolWithTag(PagedPool, size, ALLOC_TAG);
     
     if (!data) {
         ERR("out of memory\n");
+        free_traverse_ptr(&tp);
         free_traverse_ptr(&tp2);
-        return FALSE;
+        return STATUS_INSUFFICIENT_RESOURCES;
     }
     
     Status = read_file(Vcb, Vcb->root_root, inode, data, 0, ii->st_size, NULL);
     if (!NT_SUCCESS(Status)) {
         ERR("read_file returned %08x\n", Status);
         ExFreePool(data);
+        free_traverse_ptr(&tp);
         free_traverse_ptr(&tp2);
-        return FALSE;
+        return Status;
     }
     
-    num_sectors = ii->st_size / Vcb->superblock.sector_size;
+    if (size > ii->st_size)
+        RtlZeroMemory(&data[ii->st_size], size - ii->st_size);
+    
+    num_sectors = size / Vcb->superblock.sector_size;
     
     generation = *(data + (num_sectors * sizeof(UINT32)));
     
-    if (generation != Vcb->superblock.cache_generation) {
-        ERR("generation was %llx, expected %llx\n", generation, Vcb->superblock.cache_generation);
+    if (generation != fsi->generation) {
+        WARN("free space cache generation for %llx was %llx, expected %llx\n", c->offset, generation, fsi->generation);
         ExFreePool(data);
+        free_traverse_ptr(&tp);
         free_traverse_ptr(&tp2);
-        return FALSE;
+        return STATUS_NOT_FOUND;
     }
     
-    checksums = ExAllocatePoolWithTag(PagedPool, sizeof(UINT32) * num_sectors, ALLOC_TAG); // FIXME - get rid of this
+    free_traverse_ptr(&tp);
     
-    if (!checksums) {
-        ERR("out of memory\n");
+    extent_length = (num_sectors * sizeof(UINT32)) + sizeof(UINT64) + (num_entries * sizeof(FREE_SPACE_ENTRY));
+    
+    num_valid_sectors = (sector_align(extent_length, Vcb->superblock.sector_size) / Vcb->superblock.sector_size) + num_bitmaps;
+    
+    if (num_valid_sectors > num_sectors) {
+        ERR("free space cache for %llx was %llx sectors, expected at least %llx\n", c->offset, num_sectors, num_valid_sectors);
         ExFreePool(data);
         free_traverse_ptr(&tp2);
-        return FALSE;
+        return STATUS_NOT_FOUND;
     }
     
-    RtlCopyMemory(checksums, data, sizeof(UINT32) * num_sectors);
+    checksums = (UINT32*)data;
     
-    for (i = 0; i < num_sectors; i++) {
+    for (i = 0; i < num_valid_sectors; i++) {
         if (i * Vcb->superblock.sector_size > sizeof(UINT32) * num_sectors)
             crc32 = ~calc_crc32c(0xffffffff, &data[i * Vcb->superblock.sector_size], Vcb->superblock.sector_size);
         else if ((i + 1) * Vcb->superblock.sector_size < sizeof(UINT32) * num_sectors)
@@ -3104,29 +3184,46 @@ static BOOL load_stored_free_space_cache(device_extension* Vcb, chunk* c) {
         
         if (crc32 != checksums[i]) {
             WARN("checksum %llu was %08x, expected %08x\n", i, crc32, checksums[i]);
-            ExFreePool(checksums);
             ExFreePool(data);
             free_traverse_ptr(&tp2);
-            return FALSE;
+            return STATUS_NOT_FOUND;
         }
     }
     
-    ExFreePool(checksums);
-    
-#ifdef _DEBUG
     fse = (FREE_SPACE_ENTRY*)&data[(sizeof(UINT32) * num_sectors) + sizeof(UINT64)];
 
+    bmpnum = 0;
     for (i = 0; i < num_entries; i++) {
-        TRACE("(%llx,%llx,%x)\n", fse[i].offset, fse[i].size, fse[i].type);
+        if (fse[i].type == 1) {
+            Status = add_space_entry(c, fse[i].offset, fse[i].size);
+            if (!NT_SUCCESS(Status)) {
+                ERR("add_space_entry returned %08x\n", Status);
+                ExFreePool(data);
+                free_traverse_ptr(&tp2);
+                return Status;
+            }
+        } else if (fse[i].type == FREE_SPACE_BITMAP) {
+            // FIXME - make sure we don't overflow the buffer here
+            load_free_space_bitmap(Vcb, c, fse[i].offset, &data[(bmpnum + 1) * Vcb->superblock.sector_size]);
+            bmpnum++;
+        }
     }
-#endif
     
-    FIXME("FIXME - read cache\n");
+    // FIXME - merge together consecutive space entries
+    
+//     le = c->space.Flink;
+//     while (le != &c->space) {
+//         space* s = CONTAINING_RECORD(le, space, list_entry);
+//         
+//         ERR("(%llx,%llx)\n", s->offset, s->size);
+//         
+//         le = le->Flink;
+//     }
     
     ExFreePool(data);
     free_traverse_ptr(&tp2);
     
-    return FALSE;
+    return STATUS_SUCCESS;
 }
 
 static NTSTATUS load_free_space_cache(device_extension* Vcb, chunk* c) {
@@ -3138,73 +3235,80 @@ static NTSTATUS load_free_space_cache(device_extension* Vcb, chunk* c) {
     LIST_ENTRY* le;
     NTSTATUS Status;
     
-    load_stored_free_space_cache(Vcb, c);
+    Status = load_stored_free_space_cache(Vcb, c);
     
-    TRACE("generating free space cache for chunk %llx\n", c->offset);
-    
-    searchkey.obj_id = c->offset;
-    searchkey.obj_type = TYPE_EXTENT_ITEM;
-    searchkey.offset = 0;
-    
-    Status = find_item(Vcb, Vcb->extent_root, &tp, &searchkey, FALSE);
-    if (!NT_SUCCESS(Status)) {
-        ERR("error - find_item returned %08x\n", Status);
+    if (!NT_SUCCESS(Status) && Status != STATUS_NOT_FOUND) {
+        ERR("load_stored_free_space_cache returned %08x\n", Status);
         return Status;
     }
-    
-    lastaddr = c->offset;
-    
-    do {
-        if (tp.item->key.obj_id >= c->offset + c->chunk_item->size)
-            break;
+     
+    if (Status == STATUS_NOT_FOUND) {
+        TRACE("generating free space cache for chunk %llx\n", c->offset);
         
-        if (tp.item->key.obj_id >= c->offset && (tp.item->key.obj_type == TYPE_EXTENT_ITEM || tp.item->key.obj_type == TYPE_METADATA_ITEM)) {
-            if (tp.item->key.obj_id > lastaddr) {
-                s = ExAllocatePoolWithTag(PagedPool, sizeof(space), ALLOC_TAG);
-                
-                if (!s) {
-                    ERR("out of memory\n");
-                    return STATUS_INSUFFICIENT_RESOURCES;
+        searchkey.obj_id = c->offset;
+        searchkey.obj_type = TYPE_EXTENT_ITEM;
+        searchkey.offset = 0;
+        
+        Status = find_item(Vcb, Vcb->extent_root, &tp, &searchkey, FALSE);
+        if (!NT_SUCCESS(Status)) {
+            ERR("error - find_item returned %08x\n", Status);
+            return Status;
+        }
+        
+        lastaddr = c->offset;
+        
+        do {
+            if (tp.item->key.obj_id >= c->offset + c->chunk_item->size)
+                break;
+            
+            if (tp.item->key.obj_id >= c->offset && (tp.item->key.obj_type == TYPE_EXTENT_ITEM || tp.item->key.obj_type == TYPE_METADATA_ITEM)) {
+                if (tp.item->key.obj_id > lastaddr) {
+                    s = ExAllocatePoolWithTag(PagedPool, sizeof(space), ALLOC_TAG);
+                    
+                    if (!s) {
+                        ERR("out of memory\n");
+                        return STATUS_INSUFFICIENT_RESOURCES;
+                    }
+                    
+                    s->offset = lastaddr;
+                    s->size = tp.item->key.obj_id - lastaddr;
+                    s->type = SPACE_TYPE_FREE;
+                    InsertTailList(&c->space, &s->list_entry);
+                    
+                    TRACE("(%llx,%llx)\n", s->offset, s->size);
                 }
                 
-                s->offset = lastaddr;
-                s->size = tp.item->key.obj_id - lastaddr;
-                s->type = SPACE_TYPE_FREE;
-                InsertTailList(&c->space, &s->list_entry);
-                
-                TRACE("(%llx,%llx)\n", s->offset, s->size);
+                if (tp.item->key.obj_type == TYPE_METADATA_ITEM)
+                    lastaddr = tp.item->key.obj_id + Vcb->superblock.node_size;
+                else
+                    lastaddr = tp.item->key.obj_id + tp.item->key.offset;
             }
             
-            if (tp.item->key.obj_type == TYPE_METADATA_ITEM)
-                lastaddr = tp.item->key.obj_id + Vcb->superblock.node_size;
-            else
-                lastaddr = tp.item->key.obj_id + tp.item->key.offset;
+            b = find_next_item(Vcb, &tp, &next_tp, FALSE);
+            if (b) {
+                free_traverse_ptr(&tp);
+                tp = next_tp;
+            }
+        } while (b);
+        
+        if (lastaddr < c->offset + c->chunk_item->size) {
+            s = ExAllocatePoolWithTag(PagedPool, sizeof(space), ALLOC_TAG);
+            
+            if (!s) {
+                ERR("out of memory\n");
+                return STATUS_INSUFFICIENT_RESOURCES;
+            }
+            
+            s->offset = lastaddr;
+            s->size = c->offset + c->chunk_item->size - lastaddr;
+            s->type = SPACE_TYPE_FREE;
+            InsertTailList(&c->space, &s->list_entry);
+            
+            TRACE("(%llx,%llx)\n", s->offset, s->size);
         }
         
-        b = find_next_item(Vcb, &tp, &next_tp, FALSE);
-        if (b) {
-            free_traverse_ptr(&tp);
-            tp = next_tp;
-        }
-    } while (b);
-    
-    if (lastaddr < c->offset + c->chunk_item->size) {
-        s = ExAllocatePoolWithTag(PagedPool, sizeof(space), ALLOC_TAG);
-        
-        if (!s) {
-            ERR("out of memory\n");
-            return STATUS_INSUFFICIENT_RESOURCES;
-        }
-        
-        s->offset = lastaddr;
-        s->size = c->offset + c->chunk_item->size - lastaddr;
-        s->type = SPACE_TYPE_FREE;
-        InsertTailList(&c->space, &s->list_entry);
-        
-        TRACE("(%llx,%llx)\n", s->offset, s->size);
+        free_traverse_ptr(&tp);
     }
-    
-    free_traverse_ptr(&tp);
     
     // add allocated space
     
