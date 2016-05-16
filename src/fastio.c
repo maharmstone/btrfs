911dc53021cd6f9120d8f7956aa0bd7b6730a33c
Mark Harmstone <mark@harmstone.com>
2016-05-17 00:24:39 +0100

    fix fcb alignment problems in fastio.c

--- src/fastio.c
+++ src/fastio.c
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU Lesser General Public Licence
  * along with WinBtrfs.  If not, see <http://www.gnu.org/licenses/>. */
 
-#include <ntifs.h>
 #include "btrfs_drv.h"
 
 FAST_IO_DISPATCH FastIoDispatch;
