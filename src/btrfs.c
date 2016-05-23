ce03938ecf35fe69bfb0f23103c95a67e93197b5
Mark Harmstone <mark@harmstone.com>
2016-05-23 22:08:35 +0100

    set_label: move lock

--- src/btrfs.c
+++ src/btrfs.c
@@ -1391,13 +1391,13 @@ static NTSTATUS STDCALL set_label(device_extension* Vcb, FILE_FS_LABEL_INFORMATI
     
     // FIXME - check for '/' and '\\' and reject
     
-    ExAcquireResourceExclusiveLite(&Vcb->tree_lock, TRUE);
-    
 //     utf8 = ExAllocatePoolWithTag(PagedPool, utf8len + 1, ALLOC_TAG);
     
     Status = RtlUnicodeToUTF8N((PCHAR)&Vcb->superblock.label, MAX_LABEL_SIZE * sizeof(WCHAR), &utf8len, ffli->VolumeLabel, ffli->VolumeLabelLength);
     if (!NT_SUCCESS(Status))
         goto release;
+    
+    ExAcquireResourceExclusiveLite(&Vcb->tree_lock, TRUE);
     
     if (utf8len < MAX_LABEL_SIZE * sizeof(WCHAR))
         RtlZeroMemory(Vcb->superblock.label + utf8len, (MAX_LABEL_SIZE * sizeof(WCHAR)) - utf8len);
