70ac45da8997b5d52c6fd4a2a308daee73b33dc6
Mark Harmstone <mark@harmstone.com>
2016-08-06 17:51:22 +0100

    lzo compression

--- src/compress.c
+++ src/compress.c
@@ -1,5 +1,30 @@
-// Portion of the LZO code here were cribbed from code in libavcodec,
-// also under the LGPL. Thank you, Reimar Doeffinger.
+/* Copyright (c) Mark Harmstone 2016
+ * Copyright (c) Reimar Doeffinger 2006
+ * Copyright (c) Markus Oberhumer 1996
+ * 
+ * This file is part of WinBtrfs.
+ * 
+ * WinBtrfs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public Licence as published by
+ * the Free Software Foundation, either version 3 of the Licence, or
+ * (at your option) any later version.
+ * 
+ * WinBtrfs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public Licence for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public Licence
+ * along with WinBtrfs.  If not, see <http://www.gnu.org/licenses/>. */
+
+// Portion of the LZO decompression code here were cribbed from code in
+// libavcodec, also under the LGPL. Thank you, Reimar Doeffinger.
+
+// The LZO compression code comes from v0.22 of lzo, written way back in
+// 1996, and available here:
+// https://www.ibiblio.org/pub/historic-linux/ftp-archives/sunsite.unc.edu/Sep-29-1996/libs/lzo-0.22.tar.gz
+// Modern versions of lzo are licensed under the GPL, but the very oldest
+// versions are under the LGPL and hence okay to use here.
 
 #include "btrfs_drv.h"
 
@@ -20,7 +45,38 @@ typedef struct {
     UINT32 outlen;
     UINT32 outpos;
     BOOL error;
+    void* wrkmem;
 } lzo_stream;
+
+#define LZO1X_MEM_COMPRESS ((UINT32) (16384L * sizeof(UINT8*)))
+
+#define M1_MAX_OFFSET 0x0400
+#define M2_MAX_OFFSET 0x0800
+#define M3_MAX_OFFSET 0x4000
+#define M4_MAX_OFFSET 0xbfff
+
+#define MX_MAX_OFFSET (M1_MAX_OFFSET + M2_MAX_OFFSET)
+
+#define M1_MARKER 0
+#define M2_MARKER 64
+#define M3_MARKER 32
+#define M4_MARKER 16
+
+#define _DV2(p, shift1, shift2) (((( (UINT32)(p[2]) << shift1) ^ p[1]) << shift2) ^ p[0])
+#define DVAL_NEXT(dv, p) dv ^= p[-1]; dv = (((dv) >> 5) ^ ((UINT32)(p[2]) << (2*5)))
+#define _DV(p, shift) _DV2(p, shift, shift)
+#define DVAL_FIRST(dv, p) dv = _DV((p), 5)
+#define _DINDEX(dv, p) ((40799u * (dv)) >> 5)
+#define DINDEX(dv, p) (((_DINDEX(dv, p)) & 0x3fff) << 0)
+#define UPDATE_D(dict, cycle, dv, p) dict[DINDEX(dv, p)] = (p)
+#define UPDATE_I(dict, cycle, index, p) dict[index] = (p)
+
+#define LZO_CHECK_MPOS_NON_DET(m_pos, m_off, in, ip, max_offset) \
+    ((void*) m_pos < (void*) in || \
+    (m_off = (UINT8*) ip - (UINT8*) m_pos) <= 0 || \
+    m_off > max_offset)
+        
+#define LZO_BYTE(x) ((unsigned char) (x))
 
 static UINT8 lzo_nextbyte(lzo_stream* stream) {
     UINT8 c;
@@ -297,7 +353,7 @@ NTSTATUS decompress(UINT8 type, UINT8* inbuf, UINT64 inlen, UINT8* outbuf, UINT6
     }
 }
 
-NTSTATUS write_compressed_bit(fcb* fcb, UINT64 start_data, UINT64 end_data, void* data, LIST_ENTRY* changed_sector_list, PIRP Irp, LIST_ENTRY* rollback) {
+static NTSTATUS zlib_write_compressed_bit(fcb* fcb, UINT64 start_data, UINT64 end_data, void* data, LIST_ENTRY* changed_sector_list, PIRP Irp, LIST_ENTRY* rollback) {
     NTSTATUS Status;
     UINT8 compression;
     UINT64 comp_length;
@@ -422,4 +478,389 @@ NTSTATUS write_compressed_bit(fcb* fcb, UINT64 start_data, UINT64 end_data, void
     WARN("couldn't find any data chunks with %llx bytes free\n", comp_length);
 
     return STATUS_DISK_FULL;
+}
+
+static NTSTATUS lzo_do_compress(const UINT8* in, UINT32 in_len, UINT8* out, UINT32* out_len, void* wrkmem) {
+    const UINT8* ip;
+    UINT32 dv;
+    UINT8* op;
+    const UINT8* in_end = in + in_len;
+    const UINT8* ip_end = in + in_len - 9 - 4;
+    const UINT8* ii;
+    const UINT8** dict = (const UINT8**)wrkmem;
+
+    op = out;
+    ip = in;
+    ii = ip;
+
+    DVAL_FIRST(dv, ip); UPDATE_D(dict, cycle, dv, ip); ip++;
+    DVAL_NEXT(dv, ip);  UPDATE_D(dict, cycle, dv, ip); ip++;
+    DVAL_NEXT(dv, ip);  UPDATE_D(dict, cycle, dv, ip); ip++;
+    DVAL_NEXT(dv, ip);  UPDATE_D(dict, cycle, dv, ip); ip++;
+
+    while (1) {
+        const UINT8* m_pos;
+        UINT32 m_len;
+        ptrdiff_t m_off;
+        UINT32 lit, dindex;
+
+        dindex = DINDEX(dv, ip);
+        m_pos = dict[dindex];
+        UPDATE_I(dict, cycle, dindex, ip);
+
+        if (!LZO_CHECK_MPOS_NON_DET(m_pos, m_off, in, ip, M4_MAX_OFFSET) && m_pos[0] == ip[0] && m_pos[1] == ip[1] && m_pos[2] == ip[2]) {
+            lit = ip - ii;
+            m_pos += 3;
+            if (m_off <= M2_MAX_OFFSET)
+                goto match;
+
+            if (lit == 3) { /* better compression, but slower */
+                if (op - 2 <= out)
+                    return STATUS_INTERNAL_ERROR;
+                
+                op[-2] |= LZO_BYTE(3);
+                *op++ = *ii++; *op++ = *ii++; *op++ = *ii++;
+                goto code_match;
+            }
+
+            if (*m_pos == ip[3])
+                goto match;
+        }
+
+        /* a literal */
+        ++ip;
+        if (ip >= ip_end)
+            break;
+        DVAL_NEXT(dv, ip);
+        continue;
+
+        /* a match */
+match:
+        /* store current literal run */
+        if (lit > 0) {
+            UINT32 t = lit;
+
+            if (t <= 3) {
+                if (op - 2 <= out)
+                    return STATUS_INTERNAL_ERROR;
+
+                op[-2] |= LZO_BYTE(t);
+            } else if (t <= 18)
+                *op++ = LZO_BYTE(t - 3);
+            else {
+                UINT32 tt = t - 18;
+
+                *op++ = 0;
+                while (tt > 255) {
+                    tt -= 255;
+                    *op++ = 0;
+                }
+                
+                if (tt <= 0)
+                    return STATUS_INTERNAL_ERROR;
+
+                *op++ = LZO_BYTE(tt);
+            }
+            
+            do {
+                *op++ = *ii++;
+            } while (--t > 0);
+        }
+
+
+        /* code the match */
+code_match:
+        if (ii != ip)
+            return STATUS_INTERNAL_ERROR;
+        
+        ip += 3;
+        if (*m_pos++ != *ip++ || *m_pos++ != *ip++ || *m_pos++ != *ip++ ||
+            *m_pos++ != *ip++ || *m_pos++ != *ip++ || *m_pos++ != *ip++) {
+            --ip;
+            m_len = ip - ii;
+        
+            if (m_len < 3 || m_len > 8)
+                return STATUS_INTERNAL_ERROR;
+
+            if (m_off <= M2_MAX_OFFSET) {
+                m_off -= 1;
+                *op++ = LZO_BYTE(((m_len - 1) << 5) | ((m_off & 7) << 2));
+                *op++ = LZO_BYTE(m_off >> 3);
+            } else if (m_off <= M3_MAX_OFFSET) {
+                m_off -= 1;
+                *op++ = LZO_BYTE(M3_MARKER | (m_len - 2));
+                goto m3_m4_offset;
+            } else {
+                m_off -= 0x4000;
+                
+                if (m_off <= 0 || m_off > 0x7fff)
+                    return STATUS_INTERNAL_ERROR;
+                    
+                *op++ = LZO_BYTE(M4_MARKER | ((m_off & 0x4000) >> 11) | (m_len - 2));
+                goto m3_m4_offset;
+            }
+        } else {
+            const UINT8* end;
+            end = in_end;
+            while (ip < end && *m_pos == *ip)
+                m_pos++, ip++;
+            m_len = (ip - ii);
+            
+            if (m_len < 3)
+                return STATUS_INTERNAL_ERROR;
+
+            if (m_off <= M3_MAX_OFFSET) {
+                m_off -= 1;
+                if (m_len <= 33)
+                    *op++ = LZO_BYTE(M3_MARKER | (m_len - 2));
+                else {
+                    m_len -= 33;
+                    *op++ = M3_MARKER | 0;
+                    goto m3_m4_len;
+                }
+            } else {
+                m_off -= 0x4000;
+                
+                if (m_off <= 0 || m_off > 0x7fff)
+                    return STATUS_INTERNAL_ERROR;
+
+                if (m_len <= 9)
+                    *op++ = LZO_BYTE(M4_MARKER | ((m_off & 0x4000) >> 11) | (m_len - 2));
+                else {
+                    m_len -= 9;
+                    *op++ = LZO_BYTE(M4_MARKER | ((m_off & 0x4000) >> 11));
+m3_m4_len:
+                    while (m_len > 255) {
+                        m_len -= 255;
+                        *op++ = 0;
+                    }
+                    
+                    if (m_len <= 0)
+                        return STATUS_INTERNAL_ERROR;
+                    
+                    *op++ = LZO_BYTE(m_len);
+                }
+            }
+
+m3_m4_offset:
+            *op++ = LZO_BYTE((m_off & 63) << 2);
+            *op++ = LZO_BYTE(m_off >> 6);
+        }
+
+        ii = ip;
+        if (ip >= ip_end)
+            break;
+        DVAL_FIRST(dv, ip);
+    }
+
+    /* store final literal run */
+    if (in_end - ii > 0) {
+        UINT32 t = in_end - ii;
+
+        if (op == out && t <= 238)
+            *op++ = LZO_BYTE(17 + t);
+        else if (t <= 3)
+            op[-2] |= LZO_BYTE(t);
+        else if (t <= 18)
+            *op++ = LZO_BYTE(t - 3);
+        else {
+            UINT32 tt = t - 18;
+
+            *op++ = 0;
+            while (tt > 255) {
+                tt -= 255;
+                *op++ = 0;
+            }
+            
+            if (tt <= 0)
+                return STATUS_INTERNAL_ERROR;
+
+            *op++ = LZO_BYTE(tt);
+        }
+        
+        do {
+            *op++ = *ii++;
+        } while (--t > 0);
+    }
+
+    *out_len = op - out;
+    
+    return STATUS_SUCCESS;
+}
+
+static NTSTATUS lzo1x_1_compress(lzo_stream* stream) {
+    UINT8 *op = stream->out;
+    NTSTATUS Status = STATUS_SUCCESS;
+
+    if (stream->inlen <= 0)
+        stream->outlen = 0;
+    else if (stream->inlen <= 9 + 4) {
+        *op++ = LZO_BYTE(17 + stream->inlen);
+        
+        stream->inpos = 0;
+        do {
+            *op++ = stream->in[stream->inpos];
+            stream->inpos++;
+        } while (stream->inlen < stream->inpos);
+        stream->outlen = op - stream->out;
+    } else
+        Status = lzo_do_compress(stream->in, stream->inlen, stream->out, &stream->outlen, stream->wrkmem);
+    
+    if (Status == STATUS_SUCCESS) {
+        op = stream->out + stream->outlen;
+        *op++ = M4_MARKER | 1;
+        *op++ = 0;
+        *op++ = 0;
+        stream->outlen += 3;
+    }
+
+    return Status;
+}
+
+static __inline UINT32 lzo_max_outlen(UINT32 inlen) {
+    return inlen + (inlen / 16) + 64 + 3; // formula comes from LZO.FAQ
+}
+
+static NTSTATUS lzo_write_compressed_bit(fcb* fcb, UINT64 start_data, UINT64 end_data, void* data, LIST_ENTRY* changed_sector_list, PIRP Irp, LIST_ENTRY* rollback) {
+    NTSTATUS Status;
+    UINT8 compression;
+    UINT64 comp_length;
+    ULONG comp_data_len, num_pages, i;
+    UINT8* comp_data;
+    BOOL skip_compression = FALSE;
+    lzo_stream stream;
+    UINT32* out_size;
+    LIST_ENTRY* le;
+    chunk* c;
+    
+    num_pages = (sector_align(end_data - start_data, LINUX_PAGE_SIZE)) / LINUX_PAGE_SIZE;
+    
+    // Four-byte overall header
+    // Another four-byte header page
+    // Each page has a maximum size of lzo_max_outlen(LINUX_PAGE_SIZE)
+    // Plus another four bytes for possible padding
+    comp_data_len = sizeof(UINT32) + ((lzo_max_outlen(LINUX_PAGE_SIZE) + (2 * sizeof(UINT32))) * num_pages);
+    
+    comp_data = ExAllocatePoolWithTag(PagedPool, comp_data_len, ALLOC_TAG);
+    if (!comp_data) {
+        ERR("out of memory\n");
+        return STATUS_INSUFFICIENT_RESOURCES;
+    }
+    
+    stream.wrkmem = ExAllocatePoolWithTag(PagedPool, LZO1X_MEM_COMPRESS, ALLOC_TAG);
+    if (!stream.wrkmem) {
+        ERR("out of memory\n");
+        ExFreePool(comp_data);
+        return STATUS_INSUFFICIENT_RESOURCES;
+    }
+    
+    Status = excise_extents(fcb->Vcb, fcb, start_data, end_data, rollback);
+    if (!NT_SUCCESS(Status)) {
+        ERR("excise_extents returned %08x\n", Status);
+        ExFreePool(comp_data);
+        ExFreePool(stream.wrkmem);
+        return Status;
+    }
+    
+    out_size = (UINT32*)comp_data;
+    *out_size = sizeof(UINT32);
+    
+    stream.in = data;
+    stream.out = comp_data + (2 * sizeof(UINT32));
+    
+    for (i = 0; i < num_pages; i++) {
+        UINT32* pagelen = (UINT32*)(stream.out - sizeof(UINT32));
+        
+        stream.inlen = min(LINUX_PAGE_SIZE, end_data - start_data - (i * LINUX_PAGE_SIZE));
+        
+        Status = lzo1x_1_compress(&stream);
+        if (!NT_SUCCESS(Status)) {
+            ERR("lzo1x_1_compress returned %08x\n", Status);
+            skip_compression = TRUE;
+            break;
+        }
+        
+        *pagelen = stream.outlen;
+        *out_size += stream.outlen + sizeof(UINT32);
+        
+        stream.in += LINUX_PAGE_SIZE;
+        stream.out += stream.outlen + sizeof(UINT32);
+        
+        if (LINUX_PAGE_SIZE - (*out_size % LINUX_PAGE_SIZE) < sizeof(UINT32)) {
+            RtlZeroMemory(stream.out, LINUX_PAGE_SIZE - (*out_size % LINUX_PAGE_SIZE));
+            stream.out += LINUX_PAGE_SIZE - (*out_size % LINUX_PAGE_SIZE);
+            *out_size += LINUX_PAGE_SIZE - (*out_size % LINUX_PAGE_SIZE);
+        }
+    }
+    
+    ExFreePool(stream.wrkmem);
+    
+    if (skip_compression || *out_size >= end_data - start_data - fcb->Vcb->superblock.sector_size) { // compressed extent would be larger than or same size as uncompressed extent
+        ExFreePool(comp_data);
+        
+        comp_length = end_data - start_data;
+        comp_data = data;
+        compression = BTRFS_COMPRESSION_NONE;
+    } else {
+        compression = BTRFS_COMPRESSION_LZO;
+        comp_length = sector_align(*out_size, fcb->Vcb->superblock.sector_size);
+        
+        RtlZeroMemory(comp_data + *out_size, comp_length - *out_size);
+    }
+    
+    ExAcquireResourceExclusiveLite(&fcb->Vcb->chunk_lock, TRUE);
+    
+    le = fcb->Vcb->chunks.Flink;
+    while (le != &fcb->Vcb->chunks) {
+        c = CONTAINING_RECORD(le, chunk, list_entry);
+        
+        ExAcquireResourceExclusiveLite(&c->nonpaged->lock, TRUE);
+        
+        if (c->chunk_item->type == fcb->Vcb->data_flags && (c->chunk_item->size - c->used) >= comp_length) {
+            if (insert_extent_chunk(fcb->Vcb, fcb, c, start_data, comp_length, FALSE, comp_data, changed_sector_list, Irp, rollback, compression, end_data - start_data)) {
+                ExReleaseResourceLite(&c->nonpaged->lock);
+                ExReleaseResourceLite(&fcb->Vcb->chunk_lock);
+                
+                if (compression != BTRFS_COMPRESSION_NONE)
+                    ExFreePool(comp_data);
+                
+                return STATUS_SUCCESS;
+            }
+        }
+        
+        ExReleaseResourceLite(&c->nonpaged->lock);
+
+        le = le->Flink;
+    }
+    
+    if ((c = alloc_chunk(fcb->Vcb, fcb->Vcb->data_flags, rollback))) {
+        ExAcquireResourceExclusiveLite(&c->nonpaged->lock, TRUE);
+        
+        if (c->chunk_item->type == fcb->Vcb->data_flags && (c->chunk_item->size - c->used) >= comp_length) {
+            if (insert_extent_chunk(fcb->Vcb, fcb, c, start_data, comp_length, FALSE, comp_data, changed_sector_list, Irp, rollback, compression, end_data - start_data)) {
+                ExReleaseResourceLite(&c->nonpaged->lock);
+                ExReleaseResourceLite(&fcb->Vcb->chunk_lock);
+                
+                if (compression != BTRFS_COMPRESSION_NONE)
+                    ExFreePool(comp_data);
+                
+                return STATUS_SUCCESS;
+            }
+        }
+        
+        ExReleaseResourceLite(&c->nonpaged->lock);
+    }
+    
+    ExReleaseResourceLite(&fcb->Vcb->chunk_lock);
+    
+    WARN("couldn't find any data chunks with %llx bytes free\n", comp_length);
+
+    return STATUS_DISK_FULL;
+}
+
+NTSTATUS write_compressed_bit(fcb* fcb, UINT64 start_data, UINT64 end_data, void* data, LIST_ENTRY* changed_sector_list, PIRP Irp, LIST_ENTRY* rollback) {
+    if (fcb->Vcb->superblock.incompat_flags & BTRFS_INCOMPAT_FLAGS_COMPRESS_LZO) // FIXME - come up with a better way of determining when we should choose lzo
+        return lzo_write_compressed_bit(fcb, start_data, end_data, data, changed_sector_list, Irp, rollback);
+    else
+        return zlib_write_compressed_bit(fcb, start_data, end_data, data, changed_sector_list, Irp, rollback);
 }
