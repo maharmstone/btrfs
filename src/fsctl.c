7ec7ea948574bc0dae96f6a68894c5ed28a93c9b
Mark Harmstone <mark@harmstone.com>
2016-08-19 01:48:09 +0100

    create_snapshot: handle deleted fileref

--- src/fsctl.c
+++ src/fsctl.c
@@ -608,10 +608,13 @@ static NTSTATUS create_snapshot(device_extension* Vcb, PFILE_OBJECT FileObject, 
     Status = open_fileref(fcb->Vcb, &fr2, &nameus, fileref, FALSE, NULL, NULL);
     
     if (NT_SUCCESS(Status)) {
-        WARN("file already exists\n");
-        free_fileref(fr2);
-        Status = STATUS_OBJECT_NAME_COLLISION;
-        goto end3;
+        if (!fr2->deleted) {
+            WARN("file already exists\n");
+            free_fileref(fr2);
+            Status = STATUS_OBJECT_NAME_COLLISION;
+            goto end3;
+        } else
+            free_fileref(fr2);
     } else if (!NT_SUCCESS(Status) && Status != STATUS_OBJECT_NAME_NOT_FOUND) {
         ERR("open_fileref returned %08x\n", Status);
         goto end3;
