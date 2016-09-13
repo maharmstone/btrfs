f2257090db98d124fe20f51c017724f7261c53f9
Mark Harmstone <mark@harmstone.com>
2016-09-13 23:56:20 +0100

    load_chunk_root: bugfix

--- src/btrfs.c
+++ src/btrfs.c
@@ -2981,8 +2981,7 @@ static NTSTATUS STDCALL load_chunk_root(device_extension* Vcb, PIRP Irp) {
                                 RtlCopyMemory(&Vcb->devices[Vcb->devices_loaded].devitem, di, min(tp.item->size, sizeof(DEV_ITEM)));
                                 init_device(Vcb, &Vcb->devices[i], FALSE);
 
-                                if (v->seeding)
-                                    Vcb->devices[i].readonly = TRUE;
+                                Vcb->devices[i].readonly = v->seeding ? TRUE : FALSE;
 
                                 Vcb->devices[i].length = v->length;
                                 Vcb->devices_loaded++;
