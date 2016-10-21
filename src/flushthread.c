03373ab2000263af2fb70694a2def28b40556fea
Mark Harmstone <mark@harmstone.com>
2016-10-21 04:01:47 +0100

    write_trees: update tree->header.tree_id

--- src/flushthread.c
+++ src/flushthread.c
@@ -1246,6 +1246,7 @@ static NTSTATUS write_trees(device_extension* Vcb, PIRP Irp) {
 #endif
             t->header.address = t->new_address;
             t->header.generation = Vcb->superblock.generation;
+            t->header.tree_id = t->root->id;
             t->header.flags |= HEADER_FLAG_MIXED_BACKREF;
             t->header.flags &= ~HEADER_FLAG_SHARED_BACKREF;
             t->header.fs_uuid = Vcb->superblock.uuid;
