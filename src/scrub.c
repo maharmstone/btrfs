d247cb186c57db7383423922c689696f3fa71239
Mark Harmstone <mark@harmstone.com>
2017-01-15 16:22:06 +0000

    log_unrecoverable_error: support non-inline extents

--- src/scrub.c
+++ src/scrub.c
@@ -566,7 +566,67 @@ static void log_unrecoverable_error(device_extension* Vcb, UINT64 address, UINT6
     }
     
     if (rc < ei->refcount) {
-        // FIXME - non-inline extents
+        do {
+            traverse_ptr next_tp;
+            
+            if (find_next_item(Vcb, &tp, &next_tp, FALSE, NULL))
+                tp = next_tp;
+            else
+                break;
+            
+            if (tp.item->key.obj_id == address) {
+                if (tp.item->key.obj_type == TYPE_TREE_BLOCK_REF) {
+                    TREE_BLOCK_REF* tbr;
+                    
+                    if (tp.item->size < sizeof(TREE_BLOCK_REF)) {
+                        ERR("(%llx,%x,%llx) was %u bytes, expected %u\n", tp.item->key.obj_id, tp.item->key.obj_type, tp.item->key.offset,
+                                                                          tp.item->size, sizeof(TREE_BLOCK_REF));
+                        break;
+                    }
+                    
+                    tbr = (TREE_BLOCK_REF*)tp.item->data;
+                    
+                    log_tree_checksum_error(Vcb, address, devid, tbr->offset, ei2 ? ei2->level : tp.item->key.offset, ei2 ? &ei2->firstitem : NULL);
+                } else if (tp.item->key.obj_type == TYPE_EXTENT_DATA_REF) {
+                    EXTENT_DATA_REF* edr;
+                    
+                    if (tp.item->size < sizeof(EXTENT_DATA_REF)) {
+                        ERR("(%llx,%x,%llx) was %u bytes, expected %u\n", tp.item->key.obj_id, tp.item->key.obj_type, tp.item->key.offset,
+                                                                          tp.item->size, sizeof(EXTENT_DATA_REF));
+                        break;
+                    }
+                    
+                    edr = (EXTENT_DATA_REF*)tp.item->data;
+                    
+                    log_file_checksum_error(Vcb, address, devid, edr->root, edr->objid, edr->offset + address - tp.item->key.obj_id);
+                } else if (tp.item->key.obj_type == TYPE_SHARED_BLOCK_REF) {
+                    SHARED_BLOCK_REF* sbr;
+                    
+                    if (tp.item->size < sizeof(SHARED_BLOCK_REF)) {
+                        ERR("(%llx,%x,%llx) was %u bytes, expected %u\n", tp.item->key.obj_id, tp.item->key.obj_type, tp.item->key.offset,
+                                                                          tp.item->size, sizeof(SHARED_BLOCK_REF));
+                        break;
+                    }
+                    
+                    sbr = (SHARED_BLOCK_REF*)tp.item->data;
+                    
+                    log_tree_checksum_error_shared(Vcb, sbr->offset, address, devid);
+                } else if (tp.item->key.obj_type == TYPE_SHARED_DATA_REF) {
+                    SHARED_DATA_REF* sdr;
+                    
+                    if (tp.item->size < sizeof(SHARED_DATA_REF)) {
+                        ERR("(%llx,%x,%llx) was %u bytes, expected %u\n", tp.item->key.obj_id, tp.item->key.obj_type, tp.item->key.offset,
+                                                                          tp.item->size, sizeof(SHARED_DATA_REF));
+                        break;
+                    }
+                    
+                    sdr = (SHARED_DATA_REF*)tp.item->data;
+                    
+                    log_file_checksum_error_shared(Vcb, sdr->offset, address, devid, tp.item->key.obj_id);
+                }
+            } else
+                break;
+        } while (TRUE);
     }
 }
 
