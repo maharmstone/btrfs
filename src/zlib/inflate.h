5f3b40e2e2c20c73f8f3a8ff43185cd3d9c1171c
Mark Harmstone <mark@harmstone.com>
2017-03-02 03:44:42 +0000

    update zlib to 1.2.11

--- src/zlib/inflate.h
+++ src/zlib/inflate.h
@@ -1,5 +1,5 @@
 /* inflate.h -- internal inflate state definition
- * Copyright (C) 1995-2009 Mark Adler
+ * Copyright (C) 1995-2016 Mark Adler
  * For conditions of distribution and use, see copyright notice in zlib.h
  */
 
@@ -20,9 +20,9 @@
 
 /* Possible inflate modes between inflate() calls */
 typedef enum {
-    HEAD,       /* i: waiting for magic header */
+    HEAD = 16180,   /* i: waiting for magic header */
     FLAGS,      /* i: waiting for method and flags (gzip) */
-    ZLIB_TIME,       /* i: waiting for modification time (gzip) */ // renamed from TIME to avoid collision with ntdef.h
+    ZLIB_TIME,       /* i: waiting for modification time (gzip) */ // renamed from TIME to avoid collision with ntdef.h --MAH
     OS,         /* i: waiting for extra flags and operating system (gzip) */
     EXLEN,      /* i: waiting for extra length (gzip) */
     EXTRA,      /* i: waiting for extra bytes (gzip) */
@@ -79,11 +79,14 @@ typedef enum {
         CHECK -> LENGTH -> DONE
  */
 
-/* state maintained between inflate() calls.  Approximately 10K bytes. */
+/* State maintained between inflate() calls -- approximately 7K bytes, not
+   including the allocated sliding window, which is up to 32K bytes. */
 struct inflate_state {
+    z_streamp strm;             /* pointer back to this zlib stream */
     inflate_mode mode;          /* current inflate mode */
     int last;                   /* true if processing last block */
-    int wrap;                   /* bit 0 true for zlib, bit 1 true for gzip */
+    int wrap;                   /* bit 0 true for zlib, bit 1 true for gzip,
+                                   bit 2 true to validate check value */
     int havedict;               /* true if dictionary provided */
     int flags;                  /* gzip header method and flags (0 if zlib) */
     unsigned dmax;              /* zlib header max distance (INFLATE_STRICT) */
