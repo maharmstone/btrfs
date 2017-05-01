1804f92303a8f32605711a3599a33ba84b6af4be
Mark Harmstone <mark@harmstone.com>
2017-05-01 14:50:24 +0100

    fill_in_file_network_open_information: return attributes as FILE_ATTRIBUTE_NORMAL if 0

--- src/fileinfo.c
+++ src/fileinfo.c
@@ -2648,11 +2648,11 @@ static NTSTATUS STDCALL fill_in_file_network_open_information(FILE_NETWORK_OPEN_
     
     if (fcb->ads) {
         fnoi->AllocationSize.QuadPart = fnoi->EndOfFile.QuadPart = fcb->adsdata.Length;
-        fnoi->FileAttributes = fileref->parent->fcb->atts;
+        fnoi->FileAttributes = fileref->parent->fcb->atts == 0 ? FILE_ATTRIBUTE_NORMAL : fileref->parent->fcb->atts;
     } else {
         fnoi->AllocationSize.QuadPart = fcb_alloc_size(fcb);
         fnoi->EndOfFile.QuadPart = S_ISDIR(fcb->inode_item.st_mode) ? 0 : fcb->inode_item.st_size;
-        fnoi->FileAttributes = fcb->atts;
+        fnoi->FileAttributes = fcb->atts == 0 ? FILE_ATTRIBUTE_NORMAL : fcb->atts;
     }
     
     return STATUS_SUCCESS;
