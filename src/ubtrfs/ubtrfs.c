a90bba724beed9c101a50a6ec2a5f6b3ddab4303
Mark Harmstone <mark@harmstone.com>
2018-12-27 13:58:35 +0000

    ubtrfs: fix typos

--- src/ubtrfs/ubtrfs.c
+++ src/ubtrfs/ubtrfs.c
@@ -683,7 +683,7 @@ static NTSTATUS write_superblocks(HANDLE h, btrfs_dev* dev, btrfs_root* chunk_ro
     UINT64 bytes_used;
     LIST_ENTRY* le;
 
-    sblen = sizeof(sb);
+    sblen = sizeof(*sb);
     if (sblen & (sector_size - 1))
         sblen = (sblen & sector_size) + sector_size;
 
@@ -1004,7 +1004,7 @@ static BOOL is_mounted_multi_device(HANDLE h, UINT32 sector_size) {
 
     static WCHAR btrfs[] = L"\\Btrfs";
 
-    sblen = sizeof(sb);
+    sblen = sizeof(*sb);
     if (sblen & (sector_size - 1))
         sblen = (sblen & sector_size) + sector_size;
 
