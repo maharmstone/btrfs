317d7c73c3ebf6537dca7d5bd4def399f24382bf
Mark Harmstone <mark@harmstone.com>
2019-11-01 00:55:28 +0000

    rename_file_to_stream: rm superfluous UNICODE_STRING

--- src/fileinfo.c
+++ src/fileinfo.c
@@ -1706,7 +1706,6 @@ static NTSTATUS rename_file_to_stream(device_extension* Vcb, file_ref* fileref, 
                                       ULONG flags, PIRP Irp, LIST_ENTRY* rollback) {
     NTSTATUS Status;
     UNICODE_STRING fn;
-    UNICODE_STRING dsus;
     file_ref* sf = NULL;
     uint16_t newmaxlen;
     ULONG utf8len;
@@ -1744,12 +1743,10 @@ static NTSTATUS rename_file_to_stream(device_extension* Vcb, file_ref* fileref, 
     fn.Buffer = &fri->FileName[1];
     fn.Length = fn.MaximumLength = (USHORT)(fri->FileNameLength - sizeof(WCHAR));
 
-    dsus.Buffer = (WCHAR*)datasuf;
-    dsus.Length = dsus.MaximumLength = sizeof(datasuf) - sizeof(WCHAR);
-
     // remove :$DATA suffix
-    if (fn.Length >= dsus.Length && RtlCompareMemory(&fn.Buffer[(fn.Length - dsus.Length)/sizeof(WCHAR)], dsus.Buffer, dsus.Length) == dsus.Length)
-        fn.Length -= dsus.Length;
+    if (fn.Length >= sizeof(datasuf) - sizeof(WCHAR) &&
+        RtlCompareMemory(&fn.Buffer[(fn.Length - sizeof(datasuf) + sizeof(WCHAR))/sizeof(WCHAR)], datasuf, sizeof(datasuf) - sizeof(WCHAR)) == sizeof(datasuf) - sizeof(WCHAR))
+        fn.Length -= sizeof(datasuf) - sizeof(WCHAR);
 
     if (fn.Length == 0) {
         WARN("not allowing overwriting file with itself\n");
