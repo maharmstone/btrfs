4f24e50f300ac38e98239e3dff08e194731b10f7
Mark Harmstone <mark@harmstone.com>
2020-02-19 04:42:59 +0000

    btrfs.h: add new constants

--- src/btrfs.h
+++ src/btrfs.h
@@ -124,6 +124,8 @@ static const uint64_t superblock_addrs[] = { 0x10000, 0x4000000, 0x4000000000, 0
 
 #define CSUM_TYPE_CRC32C        0
 #define CSUM_TYPE_XXHASH        1
+#define CSUM_TYPE_SHA256        2
+#define CSUM_TYPE_BLAKE2        3
 
 #pragma pack(push, 1)
 
