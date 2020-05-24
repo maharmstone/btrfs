21afda5bea7edcdd3128aad3e46c885fa40ced26
Mark Harmstone <mark@harmstone.com>
2020-05-24 16:41:55 +0100

    file_create: handle SL_IGNORE_READONLY_ATTRIBUTE

--- src/create.c
+++ src/create.c
@@ -3005,8 +3005,10 @@ static NTSTATUS file_create(PIRP Irp, _Requires_lock_held_(_Curr_->tree_lock) _R
     if (Vcb->readonly)
         return STATUS_MEDIA_WRITE_PROTECTED;
 
-    if (options & FILE_DELETE_ON_CLOSE && IrpSp->Parameters.Create.FileAttributes & FILE_ATTRIBUTE_READONLY)
+    if (options & FILE_DELETE_ON_CLOSE && IrpSp->Parameters.Create.FileAttributes & FILE_ATTRIBUTE_READONLY &&
+        !(IrpSp->Flags & SL_IGNORE_READONLY_ATTRIBUTE)) {
         return STATUS_CANNOT_DELETE;
+    }
 
     if (fFsRtlGetEcpListFromIrp && fFsRtlGetNextExtraCreateParameter) {
         if (NT_SUCCESS(fFsRtlGetEcpListFromIrp(Irp, &ecp_list)) && ecp_list) {
