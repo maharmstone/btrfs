9ab86ba3d2f72d7d82e16392d99dc93ae8bae9d8
Mark Harmstone <mark@harmstone.com>
2020-09-30 13:11:17 +0100

    mingw-x86.cmake: use multilib gcc compiler

--- mingw-x86.cmake
+++ mingw-x86.cmake
@@ -6,9 +6,14 @@
 SET(CMAKE_SYSTEM_NAME Windows)
 
 # which compilers to use for C and C++
-SET(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
-SET(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
-SET(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
+SET(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+SET(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+SET(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
+
+SET(CMAKE_C_FLAGS "-m32")
+SET(CMAKE_CXX_FLAGS "-m32")
+SET(CMAKE_RC_FLAGS "-F pe-i386")
+SET(CMAKE_ASM_FLAGS "-m32")
 
 # here is the target environment located
 SET(CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32)
@@ -20,6 +25,6 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-set(CMAKE_PREFIX_PATH /usr/i686-w64-mingw32/usr/lib/cmake)
+set(CMAKE_PREFIX_PATH /usr/x86_64-w64-mingw32/usr/lib/cmake)
 
 set(CMAKE_SYSTEM_PROCESSOR x86)
