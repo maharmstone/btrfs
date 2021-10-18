e38696148814a1c5a548624a6819fb76a2a0a0c7
Mark Harmstone <mark@harmstone.com>
2021-10-18 19:27:51 +0100

    test: pass filename to NtCreateFile

--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -2,17 +2,29 @@
 
 using namespace std;
 
-static unique_handle create_file(ACCESS_MASK access, ULONG atts, ULONG share, ULONG dispo,
-                                 ULONG options) {
+static unique_handle create_file(const u16string_view& path, ACCESS_MASK access, ULONG atts, ULONG share,
+                                 ULONG dispo, ULONG options) {
     NTSTATUS Status;
     HANDLE h;
     IO_STATUS_BLOCK iosb;
+    UNICODE_STRING us;
+    OBJECT_ATTRIBUTES oa;
 
-    // FIXME - ObjectAttributes
+    oa.Length = sizeof(oa);
+    oa.RootDirectory = nullptr; // FIXME - test
+
+    us.Length = us.MaximumLength = path.length() * sizeof(char16_t);
+    us.Buffer = (WCHAR*)path.data();
+    oa.ObjectName = &us;
+
+    oa.Attributes = OBJ_CASE_INSENSITIVE; // FIXME - test
+    oa.SecurityDescriptor = nullptr; // FIXME - test
+    oa.SecurityQualityOfService = nullptr; // FIXME - test(?)
+
     // FIXME - AllocationSize
     // FIXME - EaBuffer and EaLength
 
-    Status = NtCreateFile(&h, access, nullptr, &iosb, nullptr, atts, share,
+    Status = NtCreateFile(&h, access, &oa, &iosb, nullptr, atts, share,
                           dispo, options, nullptr, 0);
 
     // FIXME
@@ -28,7 +40,7 @@ static unique_handle create_file(ACCESS_MASK access, ULONG atts, ULONG share, UL
 int main() {
     try {
         // FIXME
-        create_file(0, 0, 0, 0, 0);
+        create_file(u"\\??\\C:", 0, 0, 0, 0, 0);
     } catch (const exception& e) {
         fmt::print(stderr, "{}\n", e.what());
     }
