aac1fb8059bb7d6decc8c550eb6912d4077b2531
Mark Harmstone <mark@harmstone.com>
2021-12-02 19:45:29 +0000

    test: test creating file by creating stream

--- src/tests/streams.cpp
+++ src/tests/streams.cpp
@@ -139,7 +139,53 @@ void test_streams(const u16string& dir) {
         h.reset();
     }
 
-    // FIXME - creating file by creating stream
+    test("Create stream on non-existent file", [&]() {
+        h = create_file(dir + u"\\stream2:stream", FILE_READ_ATTRIBUTES, 0, 0, FILE_CREATE,
+                        FILE_NON_DIRECTORY_FILE, FILE_CREATED);
+    });
+
+    if (h) {
+        test("Query streams", [&]() {
+            auto items = query_streams(h.get());
+
+            if (items.size() != 2)
+                throw formatted_error("{} entries returned, expected 2", items.size());
+
+            auto& fsi1 = *static_cast<FILE_STREAM_INFORMATION*>(items[0]);
+
+            if (fsi1.StreamSize.QuadPart != 0)
+                throw formatted_error("StreamSize was {}, expected 0", fsi1.StreamSize.QuadPart);
+
+            if (fsi1.StreamAllocationSize.QuadPart != 0)
+                throw formatted_error("StreamAllocationSize was {}, expected 0", fsi1.StreamAllocationSize.QuadPart);
+
+            auto name1 = u16string_view((char16_t*)fsi1.StreamName, fsi1.StreamNameLength / sizeof(char16_t));
+
+            if (name1 != u"::$DATA")
+                throw formatted_error("StreamName was {}, expected ::$DATA", u16string_to_string(name1));
+
+            auto& fsi2 = *static_cast<FILE_STREAM_INFORMATION*>(items[1]);
+
+            if (fsi2.StreamSize.QuadPart != 0)
+                throw formatted_error("StreamSize was {}, expected 0", fsi2.StreamSize.QuadPart);
+
+            if (fsi2.StreamAllocationSize.QuadPart != 0)
+                throw formatted_error("StreamAllocationSize was {}, expected 0", fsi2.StreamAllocationSize.QuadPart);
+
+            auto name2 = u16string_view((char16_t*)fsi2.StreamName, fsi2.StreamNameLength / sizeof(char16_t));
+
+            if (name2 != u":stream:$DATA")
+                throw formatted_error("StreamName was {}, expected :stream:$DATA", u16string_to_string(name2));
+        });
+
+        h.reset();
+    }
+
+    test("Check file created for stream", [&]() {
+        create_file(dir + u"\\stream2", FILE_READ_ATTRIBUTES, 0, 0, FILE_OPEN,
+                    FILE_NON_DIRECTORY_FILE, FILE_OPENED);
+    });
+
     // FIXME - data streams on directories (can we have ::$DATA?)
     // FIXME - overwriting and superseding streams
     // FIXME - deleting streams (inc. ::$DATA)
