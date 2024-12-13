b8d72a671ca29a480d1320fa47adf853910f13bf
Mark Harmstone <mark@harmstone.com>
2024-12-13 04:39:28 +0000

    shellext: make MappingsTest show dialog box

--- src/shellext/mappings.cpp
+++ src/shellext/mappings.cpp
@@ -1,6 +1,45 @@
+#include "shellext.h"
+#include "resource.h"
 #include <windows.h>
+#include <stdexcept>
+
+using namespace std;
+
+static INT_PTR CALLBACK MappingsDlgProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    try {
+        switch (uMsg) {
+            case WM_INITDIALOG:
+                // FIXME
+            break;
+
+            case WM_COMMAND:
+                switch (HIWORD(wParam)) {
+                    case BN_CLICKED:
+                        switch (LOWORD(wParam)) {
+                            case IDOK:
+                            case IDCANCEL:
+                                EndDialog(hwndDlg, 1);
+                            return true;
+                        }
+                    break;
+                }
+            break;
+        }
+    } catch (const exception& e) {
+        error_message(hwndDlg, e.what());
+    }
+
+    return false;
+}
 
 extern "C"
-void CALLBACK MappingsTest(HWND, HINSTANCE, LPWSTR, int) {
-    // FIXME
+void CALLBACK MappingsTest(HWND hwnd, HINSTANCE, LPWSTR, int) {
+    try {
+        set_dpi_aware();
+
+        if (DialogBoxParamW(module, MAKEINTRESOURCEW(IDD_MAPPINGS), hwnd, MappingsDlgProc, 0) <= 0)
+            throw last_error(GetLastError());
+    } catch (const exception& e) {
+        error_message(hwnd, e.what());
+    }
 }
